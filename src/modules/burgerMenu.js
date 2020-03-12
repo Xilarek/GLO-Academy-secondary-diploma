14072c5f0f3b8f90b735b20a8c2d97ed9cb80b96
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2020-03-12 21:20:21 +0300

    липкое меню

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,14 +1,25 @@
 'use strict';
 const burgerMenu = () =>{
     const burgerButton = document.querySelector('.menu-button'),
-        itemMenuLink = document.querySelector('.hidden-small');
+     boxMenuLink = document.querySelector('.hidden-small'),
+     topMenu = document.querySelector('.top-menu');
 
     const windowSize = window.innerWidth;
     if(windowSize <= 768 ) {
         burgerButton.style.display = 'block';
-        itemMenuLink.style.display = 'none'; 
+        boxMenuLink.style.display = 'none'; 
     }
+    //Липкое меню
+    window.onscroll =  () => scrollFunction();
+    const sticky = topMenu.offsetTop;
     
+    function scrollFunction() {
+        if (window.pageYOffset >= sticky) {
+            topMenu.style.position = 'fixed';
+            
+          } else {
+            topMenu.style.position = 'static';
+          }
+        }
 };
-
 export default burgerMenu;
