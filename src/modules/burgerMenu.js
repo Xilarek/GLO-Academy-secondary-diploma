72f088dfa8cf8151a95ddad478706b2f9cc807f8
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2020-03-13 13:37:14 +0300

    закрытие поп-апа

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,28 +1,53 @@
 'use strict';
 const burgerMenu = () => {
     const burgerButton = document.querySelector('.menu-button'),
+        body = document.querySelector('body'),
+        popUpMenu = document.querySelector('.popup-menu'),
+        btnMenuImg = document.querySelector('.btn-img'),
+        closeBtn = document.querySelector('.close-menu-btn > img'),
+        itemUl = popUpMenu.querySelector('ul'),
+        itemPopUp = itemUl.querySelectorAll('.scroll > a'),
         boxMenuLink = document.querySelector('.hidden-small'),
         topMenu = document.querySelector('.top-menu');
 
     const windowSize = window.innerWidth;
+
     if (windowSize <= 768) {
         burgerButton.style.display = 'block';
         boxMenuLink.style.display = 'none';
     }
     //Липкое меню
-    const sticky = topMenu.offsetTop;
     window.addEventListener('scroll', () => {
 
-        console.log(sticky);
-        if (window.pageYOffset >= sticky) {
+        if (topMenu.getBoundingClientRect().top < 1) {
+
             topMenu.style.position = 'fixed';
 
-        } else {
+        } else if (topMenu.getBoundingClientRect().top === 97) {
+
             topMenu.style.position = 'static';
         }
     });
 
-    //Поп-ап
-    
+    //поп-ап
+    const handlerMenu = () => {
+        popUpMenu.style.display = 'none';
+    };
+
+    body.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target === btnMenuImg) {
+            popUpMenu.style.display = 'flex';
+        }
+        for (let i = 0; i < itemPopUp.length; i++) {
+            if (target === itemPopUp[i]) {
+                handlerMenu();
+            }
+        }
+        if (target === closeBtn) {
+            handlerMenu();
+        }
+    });
+
 };
 export default burgerMenu;
